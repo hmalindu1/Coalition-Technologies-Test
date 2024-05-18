59278af8b9c6f1c0f5d5ded6d155f7562bfa6e57
Hashan <hemachandra.hashan@gmail.com>
2024-05-18 23:48:00 +0530

    root page changed

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,9 @@
-async function getAllPatients() {
-  const username = 'coalition'
-  const password = 'skills-test'
-  const encodedCredentials = Buffer.from(`${username}:${password}`).toString(
-    'base64'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>page</div>
   )
-
-  const res = await fetch(
-    'https://fedskillstest.coalitiontechnologies.workers.dev',
-    {
-      method: 'GET',
-      headers: {
-        Authorization: `Basic ${encodedCredentials}`,
-        'Content-Type': 'application/json',
-      },
-    }
-  )
-
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
-
-  return res.json()
 }
 
-const Page = async() => {
-  const allPatientData = await getAllPatients();
-  console.log(allPatientData);
-  return <div>page</div>
-}
-
-export default Page
+export default page
