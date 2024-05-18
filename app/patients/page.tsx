59d4024073a270c9cbabbc0cf2eb07389724ce9b
Hashan <hemachandra.hashan@gmail.com>
2024-05-18 23:48:14 +0530

    patients page improved

--- app/patients/page.tsx
+++ app/patients/page.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import PatientList from "@/components/PatientsList"
 
-const patients = () => {
-  return (
-    <div>patients</div>
+async function getAllPatients() {
+  const username = 'coalition'
+  const password = 'skills-test'
+  const encodedCredentials = Buffer.from(`${username}:${password}`).toString(
+    'base64'
   )
+
+  const res = await fetch(
+    'https://fedskillstest.coalitiontechnologies.workers.dev',
+    {
+      method: 'GET',
+      headers: {
+        Authorization: `Basic ${encodedCredentials}`,
+        'Content-Type': 'application/json',
+      },
+    }
+  )
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+
+  return res.json()
 }
 
-export default patients
+const Page = async () => {
+  const allPatientData = await getAllPatients()
+//   console.log(allPatientData)
+  return <div className="my-5">
+    <PatientList patients={allPatientData} />
+  </div>
+}
+
+export default Page
