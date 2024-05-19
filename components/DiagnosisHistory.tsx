1ef7f073495f9a1e68f1e1af17657e15a3e419a9
Hashan <hemachandra.hashan@gmail.com>
2024-05-19 16:08:47 +0530

    Diagnosis history updated

--- components/DiagnosisHistory.tsx
+++ components/DiagnosisHistory.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import BloodPressureChart from './BloodPressureChart'
+import ArrowDown from '@/assets/icons/chart/ArrowDown'
+import RespiratoryRate from '@/components/RespiratoryRate'
+import Temperature from './Temperature'
+import HeartRate from './HeartRate'
 
 const DiagnosisHistory = () => {
   return (
-    <div className='w-[766px] h-[673px] p-5 bg-white rounded-xl mt-3'>
-        <h2 className='text-2xl font-bold mb-10'>Diagnosis History</h2>
-        <BloodPressureChart />
-        <div>
-            
-        </div>
+    <div className="w-[766px] h-[673px] px-5 py-2 bg-white rounded-xl mt-3">
+      <h2 className="text-2xl font-bold mt-2 mb-7">Diagnosis History</h2>
+      <BloodPressureChart />
+      <div className="mt-5 flex items-center gap-5">
+        <RespiratoryRate />
+        <Temperature />
+        <HeartRate />
+      </div>
     </div>
   )
 }
